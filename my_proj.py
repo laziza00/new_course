11233fed349e0af141357ec3e7b626a07b1c4b11
Laziza Abdulaxatova <lazizauchkunovna@gmail.com>
2021-10-10 20:44:21 +0500

    Create function

--- my_proj.py
+++ my_proj.py
@@ -7,6 +7,56 @@ mydb = mysql.connector.connect(
         database="Users",
         auth_plugin='mysql_native_password',
 )
-mycursor = mydb.cursor()
-mycursor.execute("create table kurs_ishi (id int unsigned auto_increment primary key, name varchar(20) not null, age int (3) not null, login varchar(20) not null , password varchar(20) not null)")
+# mycursor = mydb.cursor()
+# mycursor.execute("create table kurs_ishi (id int unsigned auto_increment primary key, name varchar(20) not null, age int (3) not null, login varchar(20) not null , password varchar(20) not null)")
+
+class Project:
+    def __init__(self):
+        self. name = None
+        self. age = None
+        self. login = None
+        self.password = None
+        self.options = ["1", "2", "3", "4"]
+
+    def entering_the_system(self):
+        self.initial_message()
+    def log_in(self):
+        pass
+    def registration(self):
+        pass
+    def update_login(self):
+        pass
+    def update_password(self):
+        pass
+    def delete_account(self):
+        pass
+    def log_out(self):
+        pass
+    def initial_message(self):
+        print("""Entering the system
+                 [1] - Regitr
+                 [2] - Login  
+              """)
+person = Project()
+person.entering_the_system()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
