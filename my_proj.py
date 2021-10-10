faa64cbd6bf26813015565f092f20904caf624b5
Laziza Abdulaxatova <lazizauchkunovna@gmail.com>
2021-10-10 20:30:36 +0500

    I'm create table in database

--- my_proj.py
+++ my_proj.py
@@ -1,1 +1,12 @@
-print("men boshladim")
+import mysql.connector
+
+mydb = mysql.connector.connect(
+        host="localhost",
+        user="lazii",
+        passwd="123456789",
+        database="Users",
+        auth_plugin='mysql_native_password',
+)
+mycursor = mydb.cursor()
+mycursor.execute("create table kurs_ishi (id int unsigned auto_increment primary key, name varchar(20) not null, age int (3) not null, login varchar(20) not null , password varchar(20) not null)")
+
